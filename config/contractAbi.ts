fdf857739a8a65f71512312bcdd93b3ab70d48d2
vzxh <wkq7740@gmail.com>
2024-05-15 20:48:32 +0800

    fix build error

--- config/contractAbi.ts
+++ config/contractAbi.ts
@@ -3,38 +3,39 @@ import networkWithdrawContractAbi from "./abi/networkWithdraw.json";
 import lsdTokenContractAbi from "./abi/lsdToken.json";
 import nodeDepositContractAbi from "./abi/nodeDeposit.json";
 import userDepositContractAbi from "./abi/userDeposit.json";
+import { AbiItem } from "web3-utils";
 
 /**
  * get lsdETH token contract ABI
  */
 export function getLsdEthTokenContractAbi() {
-  return lsdTokenContractAbi;
+  return lsdTokenContractAbi as AbiItem[];
 }
 
 /**
  * get ETH deposit contract ABI
  */
 export function getEthDepositContractAbi() {
-  return userDepositContractAbi;
+  return userDepositContractAbi as AbiItem[];
 }
 
 /**
  * get networkBalance contract ABI
  */
 export function getNetworkBalanceContractAbi() {
-  return networkBalanceContractAbi;
+  return networkBalanceContractAbi as AbiItem[];
 }
 
 /**
  * get nodeDeposit contract ABI
  */
 export function getNodeDepositContractAbi() {
-  return nodeDepositContractAbi;
+  return nodeDepositContractAbi as AbiItem[];
 }
 
 /**
  * get networkWithdraw contract ABI
  */
 export function getNetworkWithdrawContractAbi() {
-  return networkWithdrawContractAbi;
+  return networkWithdrawContractAbi as AbiItem[];
 }
