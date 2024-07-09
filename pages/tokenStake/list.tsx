c2019a92065ad01529f390664d2a4c8cc1273a0a
vzxh <wkq7740@gmail.com>
2024-07-09 16:31:49 +0800

    fix faq wording

--- pages/tokenStake/list.tsx
+++ pages/tokenStake/list.tsx
@@ -75,7 +75,7 @@ const TokenStakeListPage = () => {
               </a>
             </FaqItem>
 
-            <FaqItem text="What are the commissions and fees associated with staking PLS?">
+            <FaqItem text="What are the commissions and fees associated with staking ETH?">
               Staking Reward Commission: 10% of your staking reward. 5% will be
               allocated to the StaFi DAO, 5% will be allocated to validators.
             </FaqItem>
